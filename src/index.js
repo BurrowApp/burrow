4e161a59e56e01c232ae3a2bfa23b853f8d9affb
Cody Heimberger <cody.heimberger@printerlogic.com>
2018-11-09 11:23:15 -0700

    stub the rest of the endpoints

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const express = require('express')
 const app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
 app.route('/api/therm')
   .get((req, res) => {
     res.send({'termp': 33, 'humidity': 0.60})
   })
   .put((req, res) => {
-    res.send('Updated system state! (not really)')
+    res.send('Updated desired temp state! (not really)')
+  })
+
+app.route('/api/system')
+  .get((req, res) => {
+    const systemStateObject = {
+      'fan': false,
+      'system': false,
+      'mode': 'h'
+    }
+    res.send(systemStateObject)
+  })
+  .put((req, res) => {
+    res.send('updated system state (not really)')
+  })
+
+app.route('/api/schedule/away/:timestamp')
+  .post((req, res) => {
+    res.send(`Set the system to "away" ${req.params.timestamp}`)
+  })
+
+app.route('/api/weather/:zip')
+  .get((req, res) => {
+    res.send(`Get weather for ${req.params.zip}`)
   })
 
 app.listen(8080)
