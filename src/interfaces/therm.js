02f5dc7601e5bf8f8fa67c18c7e86dbd59314d88
Brance Boren <brance.boren@printerlogic.com>
2018-11-09 15:28:41 -0700

    Returning therm data as a Promise

--- src/interfaces/therm.js
+++ src/interfaces/therm.js
@@ -1,14 +1,19 @@
 const sensor = require('node-dht-sensor')
 
+const THERMOMETER_TYPE = 11
+const THERM_PIN = 18
+
 const getTemperatureData = () => {
-  sensor.read(11, 18, (err, temperature, humidity) => {
-    if (err) {
-      throw err
-    }
-    return {
-      temperature,
-      humidity
-    }
+  return new Promise((resolve, reject) => {
+    sensor.read(THERMOMETER_TYPE, THERM_PIN, (err, temperature, humidity) => {
+      if (err) {
+        reject(err)
+      }
+      resolve({
+        temperature,
+        humidity
+      })
+    })
   })
 }
 
